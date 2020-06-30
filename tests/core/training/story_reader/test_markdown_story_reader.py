7b9bea503720949c7f4699b38123d10c386ce4c3
Tobias Wochinger <t.wochinger@rasa.com>
2020-06-30 09:43:45 +0200

    adapt test to new form featurization

--- tests/core/training/story_reader/test_markdown_story_reader.py
+++ tests/core/training/story_reader/test_markdown_story_reader.py
@@ -73,7 +73,7 @@ async def test_persist_and_read_test_story(tmpdir, default_domain: Domain):
         existing_stories.discard(story_str)
 
 
-async def test_persist_form_story(tmpdir):
+async def test_persist_form_story():
     domain = Domain.load("data/test_domains/form.yml")
 
     tracker = DialogueStateTracker("", domain.slots)
@@ -93,8 +93,7 @@ async def test_persist_form_story(tmpdir):
         "    - some_form\n"
         "* stop\n"
         "    - utter_ask_continue\n"
-        "    - action_listen\n"
-        "* form: inform\n"
+        "* inform\n"
         "    - some_form\n"
         '    - form{"name": null}\n'
         "* goodbye\n"
